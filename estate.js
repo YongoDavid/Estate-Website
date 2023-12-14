b5674f28fc6ac8c8f8248b604aef3b726bdd0f71
Yongo David <davidyongo9@gmail.com>
2023-12-14 01:54:20 +0100

    on the form section

--- estate.js
+++ estate.js
@@ -1,3 +1,5 @@
-console.log('Welcome to the Realestate project')
-console.log("Testing on local machine")
-console.log('Just testing out node')
+// console.log('Welcome to the Realestate project')
+// console.log("Testing on local machine")
+// console.log('Just testing out node')
+
+// console.log('Cleaning design')
