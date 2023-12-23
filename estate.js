b20caab7d33f0a2d9f7696c78d9334fb1f96d95f
Yongo David <davidyongo9@gmail.com>
2023-12-23 03:45:34 +0100

    working on the functionality

---
+++ estate.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const itemsPerPage = 3; // Adjust as needed
+  const items = document.querySelectorAll('.AREAONE');
+  const totalItems = items.length;
+  const pageCount = Math.ceil(totalItems / itemsPerPage);
+
+  const paginationContainer = document.createElement('div');
+  paginationContainer.classList.add('pagenationArea');
+
+  for (let i = 1; i <= pageCount; i++) {
+    const pageButton = document.createElement('li');
+    pageButton.textContent = i;
+
+    if (i === 1) {
+      pageButton.classList.add('pageF');
+    } else if (i === pageCount) {
+      pageButton.classList.add('pageN');
+    } else {
+      pageButton.classList.add(`page${i}`);
+    }
+
+    pageButton.addEventListener('click', function () {
+      showPage(i);
+    });
+
+    paginationContainer.appendChild(pageButton);
+  }
+
+  document.querySelector('.Area2').appendChild(paginationContainer);
+
+  function showPage(pageNumber) {
+    const startIndex = (pageNumber - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+
+    items.forEach((item, index) => {
+      if (index >= startIndex && index < endIndex) {
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+
+    updatePaginationStyles(pageNumber);
+  }
+
+  function updatePaginationStyles(currentPage) {
+    const pageButtons = document.querySelectorAll('.pagenationArea li');
+    pageButtons.forEach((button, index) => {
+      if (index + 1 === currentPage) {
+        button.classList.add('active');
+      } else {
+        button.classList.remove('active');
+      }
+    });
+  }
+
+  // Show the first page by default
+  showPage(1);
+});
