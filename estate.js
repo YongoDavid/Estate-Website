3f6245ccab49b24815b14a71402a969307bcd26c
Yongo David <davidyongo9@gmail.com>
2023-12-30 17:32:42 +0100

    update

--- estate.js
+++ estate.js
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function () {
         // check if there is data 
         if (getData) {
             let savedata = JSON.parse(getData)
-            console.log('savedata', savedata)
+            console.log(savedata)
 
             updatePropertyList(savedata)
         } else {
@@ -51,71 +51,74 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 // function updatePropertyList(properties) {
-//     const PropertyListContainer = document.getElementById('propertyListContainer')
 
-//     properties.forEach(function (properties) {
-//         let propertyElement = document.getElementById('property1')
-//         let theaddress = document.getElementById('title-property1')
-//         let roomType = document.getElementById('type-property1')
-//         let roomPrice = document.getElementById('price-property1')
+//     const propertyListContainer = document.getElementById('propertyListContainer');
 
-//         // the footer area 
-//         let footerArea = document.getElementById('footer-property1')
-//         let footerLine = document.getElementById('fp1-line1')
-//         let footerflex = document.getElementById('flexp1-line')
-//         let footerLine2 = document.getElementById('fp1-line2')
-//         let footerLine3 = document.getElementById('fp1-line3')
+//     properties.forEach(function (property, index) {
+//         let propertyElement = document.createElement('div');
+//         propertyElement.id = `property${index + 1}`;
+        
+//         let theAddress = document.createElement('div');
+//         theAddress.id = `title-property${index + 1}`;
+//         // ... create and append other elements
 
-//     })
+//         propertyElement.appendChild(theAddress);
+//         // ... append other elements to propertyElement
+
+//         propertyListContainer.appendChild(propertyElement);
+//     });
 // }
 
-function createPropertyElement(displayData) {
-    // Create the main container div
-    let propertyElement = document.createElement('div');
-    propertyElement.classList.add('AREAONE');
+// function createPropertyElement() {
+//     // Create the main container div
+//     let propertyElement = document.createElement('div');
+//     propertyElement.classList.add('AREAONE');
 
-    // Create and style the image element
-    let imgElement = document.createElement('img');
-    imgElement.src = propertyData.imageSrc;
-    imgElement.alt = '';
-    imgElement.width = '250px';
-    imgElement.classList.add('img-property');
-    propertyElement.appendChild(imgElement);
+//     // Create and style the image element
+//     let imgElement = document.createElement('img');
+//     imgElement.src = propertyData.imageSrc;
+//     imgElement.alt = '';
+//     imgElement.width = '250px';
+//     imgElement.classList.add('img-property');
+//     propertyElement.appendChild(imgElement);
 
-    // Create and style the title element
-    let titleElement = document.createElement('h2');
-    titleElement.textContent = propertyData.title;
-    titleElement.classList.add('title-property');
-    propertyElement.appendChild(titleElement);
+//     // Create and style the title element
+//     let titleElement = document.createElement('h2');
+//     titleElement.textContent = propertyData.title;
+//     titleElement.classList.add('title-property');
+//     propertyElement.appendChild(titleElement);
 
-    // Create and style the type element
-    let typeElement = document.createElement('p');
-    typeElement.textContent = propertyData.type;
-    typeElement.classList.add('type-property');
-    propertyElement.appendChild(typeElement);
+//     // Create and style the type element
+//     let typeElement = document.createElement('p');
+//     typeElement.textContent = propertyData.type;
+//     typeElement.classList.add('type-property');
+//     propertyElement.appendChild(typeElement);
 
-    // Create and style the price element
-    let priceElement = document.createElement('div');
-    priceElement.classList.add('Price');
-    let priceParagraph = document.createElement('p');
-    priceParagraph.textContent = propertyData.price;
-    priceElement.appendChild(priceParagraph);
-    propertyElement.appendChild(priceElement);
+//     // Create and style the price element
+//     let priceElement = document.createElement('div');
+//     priceElement.classList.add('Price');
+//     let priceParagraph = document.createElement('p');
+//     priceParagraph.textContent = propertyData.price;
+//     priceElement.appendChild(priceParagraph);
+//     propertyElement.appendChild(priceElement);
 
-    // Create and style the footer area
-    let footerArea = document.createElement('div');
-    footerArea.classList.add('Area-footer-line');
-    // ... create and style other footer elements
-    propertyElement.appendChild(footerArea);
+//     // Create and style the footer area
+//     let footerArea = document.createElement('div');
+//     footerArea.classList.add('Area-footer-line');
+//     // ... create and style other footer elements
+//     propertyElement.appendChild(footerArea);
 
-    // Create and style the footer content
-    let footerContent = document.createElement('div');
-    footerContent.classList.add('Area-footer');
-    // ... create and style other footer content elements
-    propertyElement.appendChild(footerContent);
+//     // Create and style the footer content
+//     let footerContent = document.createElement('div');
+//     footerContent.classList.add('Area-footer');
+//     // ... create and style other footer content elements
+//     propertyElement.appendChild(footerContent);
 
-    // Add the property element to the document
-    let propertyListContainer = document.getElementById('propertyListContainer');
-    propertyListContainer.appendChild(propertyElement);
-}
-createPropertyElement()
+//     // Add the property element to the document
+//     let propertyListContainer = document.getElementById('propertyListContainer');
+//     propertyListContainer.appendChild(propertyElement);
+// }
+// createPropertyElement()
+
+
+// // I NEED TO LINK ALL THIS CODES TOGETHER AND MAKE SURE THEY ARE ALL WORKING 
