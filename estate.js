8cbc9bc91e7d27f11c9259f40066d1ddd6b109ab
Yongo David <davidyongo9@gmail.com>
2023-12-30 17:40:01 +0100

    modified the files estate.js

--- estate.js
+++ estate.js
@@ -50,75 +50,75 @@ document.addEventListener('DOMContentLoaded', function () {
     form.addEventListener('click' ,processForm)
 });
 
-// function updatePropertyList(properties) {
+function updatePropertyList(properties) {
 
-//     const propertyListContainer = document.getElementById('propertyListContainer');
+    const propertyListContainer = document.getElementById('propertyListContainer');
 
-//     properties.forEach(function (property, index) {
-//         let propertyElement = document.createElement('div');
-//         propertyElement.id = `property${index + 1}`;
+    properties.forEach(function (property, index) {
+        let propertyElement = document.createElement('div');
+        propertyElement.id = `property${index + 1}`;
         
-//         let theAddress = document.createElement('div');
-//         theAddress.id = `title-property${index + 1}`;
-//         // ... create and append other elements
+        let theAddress = document.createElement('div');
+        theAddress.id = `title-property${index + 1}`;
+        // ... create and append other elements
 
-//         propertyElement.appendChild(theAddress);
-//         // ... append other elements to propertyElement
+        propertyElement.appendChild(theAddress);
+        // ... append other elements to propertyElement
 
-//         propertyListContainer.appendChild(propertyElement);
-//     });
-// }
+        propertyListContainer.appendChild(propertyElement);
+    });
+}
 
-// function createPropertyElement() {
-//     // Create the main container div
-//     let propertyElement = document.createElement('div');
-//     propertyElement.classList.add('AREAONE');
+function createPropertyElement() {
+    // Create the main container div
+    let propertyElement = document.createElement('div');
+    propertyElement.classList.add('AREAONE');
 
-//     // Create and style the image element
-//     let imgElement = document.createElement('img');
-//     imgElement.src = propertyData.imageSrc;
-//     imgElement.alt = '';
-//     imgElement.width = '250px';
-//     imgElement.classList.add('img-property');
-//     propertyElement.appendChild(imgElement);
+    // Create and style the image element
+    let imgElement = document.createElement('img');
+    imgElement.src = propertyData.imageSrc;
+    imgElement.alt = '';
+    imgElement.width = '250px';
+    imgElement.classList.add('img-property');
+    propertyElement.appendChild(imgElement);
 
-//     // Create and style the title element
-//     let titleElement = document.createElement('h2');
-//     titleElement.textContent = propertyData.title;
-//     titleElement.classList.add('title-property');
-//     propertyElement.appendChild(titleElement);
+    // Create and style the title element
+    let titleElement = document.createElement('h2');
+    titleElement.textContent = propertyData.title;
+    titleElement.classList.add('title-property');
+    propertyElement.appendChild(titleElement);
 
-//     // Create and style the type element
-//     let typeElement = document.createElement('p');
-//     typeElement.textContent = propertyData.type;
-//     typeElement.classList.add('type-property');
-//     propertyElement.appendChild(typeElement);
+    // Create and style the type element
+    let typeElement = document.createElement('p');
+    typeElement.textContent = propertyData.type;
+    typeElement.classList.add('type-property');
+    propertyElement.appendChild(typeElement);
 
-//     // Create and style the price element
-//     let priceElement = document.createElement('div');
-//     priceElement.classList.add('Price');
-//     let priceParagraph = document.createElement('p');
-//     priceParagraph.textContent = propertyData.price;
-//     priceElement.appendChild(priceParagraph);
-//     propertyElement.appendChild(priceElement);
+    // Create and style the price element
+    let priceElement = document.createElement('div');
+    priceElement.classList.add('Price');
+    let priceParagraph = document.createElement('p');
+    priceParagraph.textContent = propertyData.price;
+    priceElement.appendChild(priceParagraph);
+    propertyElement.appendChild(priceElement);
 
-//     // Create and style the footer area
-//     let footerArea = document.createElement('div');
-//     footerArea.classList.add('Area-footer-line');
-//     // ... create and style other footer elements
-//     propertyElement.appendChild(footerArea);
+    // Create and style the footer area
+    let footerArea = document.createElement('div');
+    footerArea.classList.add('Area-footer-line');
+    // ... create and style other footer elements
+    propertyElement.appendChild(footerArea);
 
-//     // Create and style the footer content
-//     let footerContent = document.createElement('div');
-//     footerContent.classList.add('Area-footer');
-//     // ... create and style other footer content elements
-//     propertyElement.appendChild(footerContent);
+    // Create and style the footer content
+    let footerContent = document.createElement('div');
+    footerContent.classList.add('Area-footer');
+    // ... create and style other footer content elements
+    propertyElement.appendChild(footerContent);
 
-//     // Add the property element to the document
-//     let propertyListContainer = document.getElementById('propertyListContainer');
-//     propertyListContainer.appendChild(propertyElement);
-// }
-// createPropertyElement()
+    // Add the property element to the document
+    let propertyListContainer = document.getElementById('propertyListContainer');
+    propertyListContainer.appendChild(propertyElement);
+}
+createPropertyElement()
 
 
 // // I NEED TO LINK ALL THIS CODES TOGETHER AND MAKE SURE THEY ARE ALL WORKING 
