ee64a49b4c35192b69f27d04eeed33e6ec1b8b59
Yongo David <davidyongo9@gmail.com>
2024-01-05 22:07:26 +0100

    server package that i might delete

--- Server/server.js
+++ Server/server.js
@@ -1,35 +1,65 @@
-const http = require('http')
-const PORT = 3000
-const server = http.createServer((req, res) => {
-    console.log('Request Made')
-    res.setHeader('Content-Type', 'text/plain')
-    console.log(req.url)
-    switch (req.url) {
-        case '/':
-            res.writeHead(200)
-            res.write('Homepage')
-            res.end()
-            break;
-        case '/Properties':
-            res.writeHead(200)
-            res.write('List of Properties')
-            res.end()
-            break;
-        case '/Form':
-            res.writeHead(200)
-            res.write('Form')
-            res.end()
-        default:
-            res.writeHead(404)
-            res.write('404 page')
-            res.end()
-            break;
-    }
-})
+const express = require('express')
+const app = express
+const port = 6363
 
-server.listen(3000, 'localhost', () => {
-    console.log(`listing on ${PORT}`)
-})
+app.listen(localhost, () => console.log(`Server is runnig on port: ${port}`) )
 
 
-// NEED TO LINK DATA GOTTEN FRM FORM TO SERVER 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const http = require('http')
+// const PORT = 3000
+// const server = http.createServer((req, res) => {
+//     console.log('Request Made')
+//     res.setHeader('Content-Type', 'text/plain')
+//     console.log(req.url)
+//     switch (req.url) {
+//         case '/':
+//             res.writeHead(200)
+//             res.write('Homepage')
+//             res.end()
+//             break;
+//         case '/Properties':
+//             res.writeHead(200)
+//             res.write('List of Properties')
+//             res.end()
+//             break;
+//         case '/Form':
+//             res.writeHead(200)
+//             res.write('Form')
+//             res.end()
+//         default:
+//             res.writeHead(404)
+//             res.write('404 page')
+//             res.end()
+//             break;
+//     }
+// })
+
+// server.listen(3000, 'localhost', () => {
+//     console.log(`listing on ${PORT}`)
+// })
+
+
+// // NEED TO LINK DATA GOTTEN FRM FORM TO SERVER 
