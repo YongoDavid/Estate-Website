47844765626b22360e432be68f2c92b7009fd002
Yongo David <davidyongo9@gmail.com>
2024-01-10 15:11:02 +0100

    server installation test

---
+++ Server/app.js
@@ -0,0 +1,27 @@
+console.log('running another server for this')
+
+// const http = require('http')
+// const server = http.createServer((req, res) => {
+//     res.setHeader('Content-Type' , 'Text/plain')
+// })
+
+// server.listen(1000, 'localhost', () => {
+//     console.log('server listening on port 1000')
+// })
+
+const http = require('http')
+
+const PORT = 3000
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'Text/plain')
+    res.writeHead(200)
+    res.write('Welcome')
+    res.end()
+})
+
+server.listen(3000, 'localhost' , () => {
+    console.log('Listening on port 3000')
+})
+
+// TRY SETTING UP SERVER 
